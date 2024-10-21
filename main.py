04b0d19bb751e8c9bbf8429b757b4a48763aad8b
dan5e3s6ares <daniel.martins.ext@tooseguros.com.br>
2024-10-21 08:31:04 -0300

    Add Step Path Parameters

--- main.py
+++ main.py
@@ -1,8 +1,31 @@
+from enum import Enum
+
 from fastapi import FastAPI
 
 app = FastAPI()
 
 
+class ItemsEnum(str, Enum):
+    alexnet = "alexnet"
+    resnet = "resnet"
+    lenet = "lenet"
+
+
 @app.get("/")
 async def root():
     return {"message": "Hello World"}
+
+
+@app.get("/items_any_type/{item_id}")
+async def read_item(item_id):
+    return {"item_id": item_id}
+
+
+@app.get("/items_integer/{item_id}")
+async def read_item_integer(item_id: int):
+    return {"item_id": item_id}
+
+
+@app.get("/items_enum/{items_enum}")
+async def get_items_enum(items_enum: ItemsEnum):
+    return {"Item Enum": items_enum}
